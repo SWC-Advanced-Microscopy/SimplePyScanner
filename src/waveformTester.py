72c96e83285ced318d937a1c27977a2fa18b6497
Rob Campbell <git@raacampbell.com>
2020-09-17 13:29:50 +0100

    waveformTester ready for trial run

--- src/waveformTester.py
+++ src/waveformTester.py
@@ -92,27 +92,26 @@ class waveformTester():
     _phase_plot = [] # Handle fo the plot of AI1 as a function of AI0
     _plt_ao0 = [] # AO0 plot data
     _plt_ai0 = [] # AI0 plot data
-    _plt_xy  = [] # AO0 vs AO1 plot data
+    _plt_phase  = [] # AO0 vs AO1 plot data
 
+    _read_number = 0 # counter for the number of times the DAQmx callback is run
 
-
-    def __init(self,dev_name=''):
+    def __init__(self,dev_name=''):
 
         # Optionally replace device name if needed
         if 'Dev' in dev_name:
             self.dev_name = dev_name
 
+        # Build the figure window
+        self.build_figure_window()
+
         # Call a method to connect to the DAQ. If the following line fails, the Tasks are
         # cleaned up gracefully and the object is deleted. This is all done by the method
         # call and by the destructor
-        ##self.connect_to_daq()
-
-        # Build the figure window
-        #self.build_figure_window()
+        self.connect_to_daq()
 
 
-        # Start the acquisition
-        ##self.start()
+        self.start()
         print('Close figure to quit acquisition')
 
 
@@ -127,184 +126,215 @@ class waveformTester():
         ##self.ao_task.close()
     #close destructor
 
-
     def connect_to_daq(self):
-        # Note how we try to name the methods in the most descriptive way possible
-        # Attempt to connect to the DAQ and set it up. If we fail, we close the 
-        # connection to the DAQ and tidy up
-        try:
-            # Create separate DAQmx tasks for the AI and AO
-            self.ai_task = nidaqmx.Task('signalReceiver')
-            self.ao_task = nidaqmx.Task('waveformMaker')
+        # First generate scan waveforms, as they properties will affect some variables, such as
+        # buffer sizes. 
 
-            #  Set up analog input and output voltage channels, digitizing over +/- 5V
-            self.ai_task.ai_channels.add_ai_voltage_chan(self.dev_name+'/ai0:1', min_val=-5, max_val=5)
-            self.ao_task.ao_channels.add_ao_voltage_chan(self.dev_name+'/ao0:1', min_val=-5, max_val=5)
+        self.generate_scan_waveform() #This will populate the waveform property
+        l_wav = len(self.waveform)
 
+        print('Connecting to DAQ')
 
-            # * Set up the AI task
+        # Create separate DAQmx tasks for the AI and AO
+        self.ai_task = nidaqmx.Task('signalReceiver')
+        self.ao_task = nidaqmx.Task('waveformMaker')
 
-            # Configure the sampling rate and the number of samples so that we are reading back data at the end of each waveform
-            self.generate_scan_waveform() #This will populate the waveforms property
-            buffer_size = len(self.waveform)*100
-
-            # Set timing and use AO clock for AI
-            self.ai_task.timing.cfg_samp_clk_timing(self.sample_rate, \
-                                source= '/%s/ao/SampleClock' % self.dev_name, \
-                                samps_per_chan=buffer_size, \
-                                sample_mode=AcquisitionType.CONTINUOUS)
-
-            # NOTE: must explicitly set the input buffer so that it's a multiple
-            # of the number of samples per frame. Setting the samples per channel 
-            # (above) does not achieve this.
-            self.ai_task.in_stream.input_buf_size = buffer_size
+        #  Set up analog input and output voltage channels, digitizing over +/- 5V
+        # Channel 0 is the recorded copy of the AO signal. Channel 1 is the scanner feedback.
+        self.ai_task.ai_channels.add_ai_voltage_chan(self.dev_name+'/ai0:1', min_val=-5, max_val=5)
+        self.ao_task.ao_channels.add_ao_voltage_chan(self.dev_name+'/ao0', min_val=-5, max_val=5)
 
 
-            # Call an anonymous function to read from the AI buffer and plot the images once per frame
-            self.ai_task.register_every_n_samples_acquired_into_buffer_event(buffer_size, self.read_and_display_data)
+        # * Set up the AI task
+
+        # Configure the sampling rate and the number of samples so that we are reading back data 
+        # at the end of each waveform
+
+        # Set timing and use AO clock for AI
+        self.ai_task.timing.cfg_samp_clk_timing(self.sample_rate, \
+                            source= '/%s/ao/SampleClock' % self.dev_name, \
+                            samps_per_chan=l_wav*100, \
+                            sample_mode=AcquisitionType.CONTINUOUS)
+
+        # NOTE: must explicitly set the input buffer so that it's a multiple
+        # of the number of samples per frame. Setting the samples per channel 
+        # (above) does not achieve this.
+        self.ai_task.in_stream.input_buf_size = l_wav*100
 
 
-            # * Set up the AO task
-            # Set the size of the output buffer
-            self.ao_task.cfgSampClkTiming(self.sample_rate, 'DAQmx_Val_ContSamps', size(self.waveform,1))
-
-            # TODO -- just link the rates
-            #if self.ao_task.sampClkRate != self.ai_task.sampClkRate:
-            # TODO -- just link the rates
-            #print(['WARNING: AI task sample clock rate does not match AO task sample clock rate. Scan lines will precess.\n', ...
-            #       'This issue is corrected in polishedScanner, which uses a shared sample clock between AO and AI'])
+        # Call an anonymous function to read from the AI buffer and plot the images once per frame
+        print(self.sample_rate)
+        print('Running callback every %0.2f seconds' % (l_wav/self.sample_rate) )
+        self.ai_task.register_every_n_samples_acquired_into_buffer_event(l_wav, self.read_and_display_data)
 
 
-            # Allow sample regeneration (buffer is circular)
-            self.ao_task.out_stream.regen_mode = RegenerationMode.ALLOW_REGENERATION
+        # * Set up the AO task
+        # Set the size of the output buffer
+        self.ao_task.timing.cfg_samp_clk_timing(self.sample_rate, \
+                sample_mode=AcquisitionType.CONTINUOUS, \
+                samps_per_chan=len(self.waveform))
 
-            # Write the waveform to the buffer with a 5 second timeout in case it fails
-            self.ao_task.write(self.waveform, timeout=5)
 
-            # Configure the AO task to start as soon as the AI task starts
-            self.ao_task..triggers.start_trigger.cfg_dig_edge_start_trig( '/' + self.dev_name + '/ai/StartTrigger' )
-        except:
-                #Tidy up if we fail
-            self.delete
+        # Allow sample regeneration (buffer is circular)
+        self.ao_task.out_stream.regen_mode = RegenerationMode.ALLOW_REGENERATION
+
+
+        # Write the waveform to the buffer with a 5 second timeout in case it fails
+        self.ao_task.write(self.waveform, timeout=5)
+
+
+        # Configure the AO task to start as soon as the AI task starts
+        self.ao_task.triggers.start_trigger.cfg_dig_edge_start_trig( '/' + self.dev_name + '/ai/StartTrigger' )
+ 
         # close connect_to_daq
 
 
     def build_figure_window(self):
-
+        print("Building figure window")
         self._app = QtGui.QApplication([])
         self._win = pg.GraphicsLayoutWidget(show=True)
         self._main_plot = self._win.addPlot()
-        self._plt_ao0 = self._main_plot(pen='k')
-        self._plt_ai0 = self._main_plot(pen='r')
+        self._plt_ao0 = self._main_plot.plot(pen='w',name='AO0')
+        self._plt_ai0 = self._main_plot.plot(pen='r',name='AI0')
+
+        self._main_plot.setYRange(-self.galvo_amplitude*1.15,self.galvo_amplitude*1.15)
+        self._main_plot.getAxis('left').setLabel('Voltage (V)')
+        self._main_plot.getAxis('bottom').setLabel('Time (ms)')
 
 
-        ##set(self.hFig, 'CloseRequestFcn', @self.windowCloseFcn)
 
-        #Make an empty axis and fill with blank data
-        self.hAxes = axes('Parent', self.hFig, 'Position', [0.09,0.1,0.89,0.88], 'NextPlot', 'add', \
-                'YLim',[-self.galvo_amplitude*1.15,self.galvo_amplitude*1.15])
-        self.hAxes.XLabel.String = 'Time (ms)';
-        self.hAxes.YLabel.String = 'Voltage';
-        self.hPltDataAO0 = plot(self.hAxes, zeros(100,1), '-k') # This plot object holds data from AO0
-        self.hPltDataAO1 = plot(self.hAxes, zeros(100,1), '-r') # This plot object holds data from AO1
+        self._phase_plot = self._win.addPlot()
+        self._plt_phase = self._phase_plot.plot(pen='b')
+
+        self._phase_plot.getAxis('left').setLabel('AO0')
+        self._phase_plot.getAxis('bottom').setLabel('AI0')
 
 
-        set(self.hAxes, 'XLim', [0,length(self.waveform)/self.sample_rate*1E3], 'Box', 'on')
+
+
+        # Set window title
+        #set(self.hFig,'Name', sprintf('Close figure to stop acquisition - waveform frequency=#0.1f HZ', 1/self.linePeriod) )
+
+
+        ## Plot empty data (may not be needed?)
+        ##self.hPltDataAO0 = plot(self.hAxes, zeros(100,1), '-k') # This plot object holds data from AO0
+        ##self.hPltDataAO1 = plot(self.hAxes, zeros(100,1), '-r') # This plot object holds data from AO1
+
+        # Axis xlimits
+        ##set(self.hAxes, 'XLim', [0,length(self.waveform)/self.sample_rate*1E3], 'Box', 'on')
         ##grid on
-        legend('command','galvo position')
+
+        # Legend
+        #legend('command','galvo position')
 
 
         # Start of code for making the blue inset plot
-        self.hAxesXY = axes('Parent', self.hFig, 'Position', [0.8,0.1,0.2,0.2], 'NextPlot', 'add')
-        self.hPltDataXY = plot(self.hAxesXY, zeros(100,1), '-b.')
-        set(self.hAxesXY, 'XTickLabel', [], 'YTickLabel',[], \
-            'YLim',[-self.galvo_amplitude*1.15,self.galvo_amplitude*1.15],'XLim',[-self.galvo_amplitude*1.15,self.galvo_amplitude*1.15])
+        ##self.hAxesXY = axes('Parent', self.hFig, 'Position', [0.8,0.1,0.2,0.2], 'NextPlot', 'add')
+        ##self.hPltDataXY = plot(self.hAxesXY, zeros(100,1), '-b.')
+        ##set(self.hAxesXY, 'XTickLabel', [], 'YTickLabel',[], \
+        ##    'YLim',[-self.galvo_amplitude*1.15,self.galvo_amplitude*1.15],'XLim',[-self.galvo_amplitude*1.15,self.galvo_amplitude*1.15])
 
         #Add "crosshairs" to show x=0 and y=0
-        plot(self.hAxesXY, [-self.galvo_amplitude*1.15,self.galvo_amplitude*1.15], [0,0], ':k')
-        plot(self.hAxesXY, [0,0], [-self.galvo_amplitude*1.15,self.galvo_amplitude*1.15], ':k')
+        ##plot(self.hAxesXY, [-self.galvo_amplitude*1.15,self.galvo_amplitude*1.15], [0,0], ':k')
+        ##plot(self.hAxesXY, [0,0], [-self.galvo_amplitude*1.15,self.galvo_amplitude*1.15], ':k')
 
-        self.hAxesXY.Color=[0.8,0.8,0.95,0.75]; #blue background  and transparent (4th input, an undocumented MATLAB feature)
+        ##self.hAxesXY.Color=[0.8,0.8,0.95,0.75]; #blue background  and transparent (4th input, an undocumented MATLAB feature)
         ##grid on
         ##box on
         ##axis square
         # End of code for making the inset plot
 
-        set(self.hFig,'Name', sprintf('Close figure to stop acquisition - waveform frequency=#0.1f HZ', 1/self.linePeriod) )
+
+        self._win.show()
+
+    def start(self):
+        # This method starts acquisition on the AO then the AI task. 
+        # Acquisition begins immediately since there are no external triggers.
+        print('Starting tasks')
+        self.ao_task.start()
+        self.ai_task.start()
+
+    #close start
+
+
+    def stop(self):
+        # Stop the AI and then AO tasks
+        print('Stopping the scanning AI and AO tasks')
+        self.ai_task.stop()
+        self.ao_task.stop()
+    #close stop
+
+
+    def generate_scan_waveform(self):
+        # This method builds a simple ("unshaped") galvo waveform and stores it in the self.waveform
+
+        if self.waveform_type == 'sawtooth':
+            print('Generating a sawtooth')
+            # The X waveform goes from +galvo_amplitude to -galvo_amplitude over the course of one line:
+            xWaveform = np.linspace(-self.galvo_amplitude, self.galvo_amplitude, self.pixels_per_line) 
+
+            # Repeat the X waveform a few times to ease visualisation on-screen
+            xWaveform = np.tile(xWaveform, 1, self.num_reps_per_acq)
+            self.waveform = xWaveform; # Assign to the waveform property which gets written to the DAQ
+
+        elif self.waveform_type == 'sine':
+            print('Generating a sine wave')
+            # sine wave
+            self.waveform = self.galvo_amplitude *  np.sin(np.linspace(-np.pi*self.num_reps_per_acq, \
+                                                            np.pi*self.num_reps_per_acq, \
+                                                            self.pixels_per_line*self.num_reps_per_acq))
+
+        #Report waveform properties
+        if False:
+            print(np.size(self.waveform))
+            print(self.waveform)
+            import matplotlib.pyplot as plt 
+            plt.plot(self.waveform)
+            plt.show()
+        print('Generated a waveform of length %d and a line period of %0.3f ms (%0.1f Hz)' % \
+               (self.pixels_per_line, self.line_period()*1E3, 1/self.line_period()))
+    #close generate_scan_waveform
+
+
+    def read_and_display_data(self,tTask, event_type, num_samples, callback_data):
+        # This callback method is run each time data have been acquired.
+        data = self.ai_task.read(number_of_samples_per_channel=len(self.waveform))
+        self._win.setWindowTitle('Plot update #%d' % self._read_number)
+        self._read_number += 1
+        self._plt_ao0.setData(data[0])
+        self._plt_ai0.setData(data[1])
+        self._plt_phase.setData(data[0],data[1])
+
+
+        # Read data off the DAQ
+        #inData = readAnalogData(src,src.everyNSamples,'Scaled')
+
+        #timeAxis = np.arange(0,len(inData)-1) / self.sample_rate*1E3;
+        #self.hPltDataAO0.YData = inData[:,1]
+        #self.hPltDataAO0.XData=timeAxis
+        #Scale the feedback signal so it's the same amplitude as the command
+        #scaleFactor = max(inData[:,1]) / max(inData[:,2])
+        #self.hPltDataAO1.YData = inData[:,2]*scaleFactor
+        #self.hPltDataAO1.XData=timeAxis;
+
+        #self.hPltDataXY.YData = inData[:,2]*scaleFactor
+        #self.hPltDataXY.XData = inData[:,1]
+
+        return 0
+    #close read_and_display_data
+
+
+    def line_period(self):
+        if len(self.waveform)==0:
+            LP=[]
+        else:
+            LP = len(self.waveform) / (self.sample_rate*self.num_reps_per_acq)
+
+        return LP
+    # close line_period
 
 
 
-        def start(self):
-            # This method starts acquisition on the AO then the AI task. 
-            # Acquisition begins immediately since there are no external triggers.
-            try:
-                self.ao_task.start()
-                self.ai_task.start()
-            except:
-                #Tidy up if we fail
-                self.delete
-
-        #close start
-
-
-        def stop(self):
-            # Stop the AI and then AO tasks
-            print('Stopping the scanning AI and AO tasks')
-            self.ai_task.stop()
-            self.ao_task.stop()
-        #close stop
-
-
-        def generate_scan_waveform(self):
-            # This method builds a simple ("unshaped") galvo waveform and stores it in the self.waveform
-
-            if self.waveform_type == 'sawtooth':
-                # The X waveform goes from +galvo_amplitude to -galvo_amplitude over the course of one line:
-                xWaveform = linspace(-self.galvo_amplitude, self.galvo_amplitude, self.pixels_per_line) 
-
-                # Repeat the X waveform a few times to ease visualisation on-screen
-                xWaveform = repmat(xWaveform, 1, self.num_reps_per_acq)  # NOTE! It's a column vector [CHECK! TODO]
-                
-                self.waveform = xWaveform; # Assign to the waveform property which gets written to the DAQ
-
-            elif self.waveform_type == 'sine':
-                # sine wave
-                self.waveform = self.galvo_amplitude *  sin(linspace(-pi*self.num_reps_per_acq, pi*self.num_reps_per_acq, self.pixels_per_line*self.num_reps_per_acq))
-
-            #Report waveform properties
-            print('Scanning with a waveform of length #d and a line period of #0.3f ms (#0.1f Hz)' % \
-                  (self.pixels_per_line, self.linePeriod*1E3, 1/self.linePeriod))
-        #close generate_scan_waveform
-
-
-        def read_and_display_data(self,src,evnt):
-            # This callback method is run each time data have been acquired.
-            # This happens because the of the listener set up in the method connect_to_daq
-            # on the "self.ai_task.registerEveryNSamplesEvent" line.
-
-            # Read data off the DAQ
-            inData = readAnalogData(src,src.everyNSamples,'Scaled')
-
-            timeAxis = np.arange(0,len(inData)-1) / self.sample_rate*1E3;
-            self.hPltDataAO0.YData = inData[:,1]
-            self.hPltDataAO0.XData=timeAxis
-            #Scale the feedback signal so it's the same amplitude as the command
-            scaleFactor = max(inData[:,1]) / max(inData[:,2])
-            self.hPltDataAO1.YData = inData[:,2]*scaleFactor
-            self.hPltDataAO1.XData=timeAxis;
-
-            self.hPltDataXY.YData = inData[:,2]*scaleFactor
-            self.hPltDataXY.XData = inData[:,1]
-        #close read_and_display_data
-
-
-
-        def linePeriod(self):
-            if len(self.waveform)==0:
-               LP=[]
-            else:
-                LP = length(self.waveform) / (self.sample_rate*self.num_reps_per_acq)
-
-            return LP
-        # close linePeriod
-
+if __name__ == '__main__':
+    W=waveformTester()
+    input('Press return to stop\n')
